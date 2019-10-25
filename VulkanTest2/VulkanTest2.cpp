6c3e3103d21d2efd04d1d1260b7cd32e038076be
jodavis <jodavis42@gmail.com>
2019-10-24 19:59:40 -0700

    Added glm

--- VulkanTest2/VulkanTest2.cpp
+++ VulkanTest2/VulkanTest2.cpp
@@ -4,6 +4,11 @@
 #include <GLFW/glfw3.h>
 //#include <vulkan/vulkan.h>
 
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
+
 #include <iostream>
 #include <stdexcept>
 #include <functional>
